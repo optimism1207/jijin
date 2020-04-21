c6327d6043883b09f18172218f71828d7b8514b2
gx <gx@localhost.localdomain>
2020-04-21 13:43:19 +0800

    增加买入手续费的计算

--- jjyk.py
+++ jjyk.py
@@ -1,5 +1,6 @@
 import numpy
 import math
+from decimal import Decimal
 import argparse
 
 def default():
@@ -13,13 +14,17 @@ def specific(i):
     f = i/100/(1-i/100)*100
     print("当前亏损：" + '%.2f' % i + "%", "回本需涨：" + '%.2f' % f + "%")
 
-def cost_hold_method(min, max, step, current_value, total_now, share_hold_now): 
+def cost_hold_method(min, max, step, current_value, purchase_fee, total_now, share_hold_now): 
     for amount_add in range(min , max + 1 ,step): 
-        cost_hold = (total_now + amount_add) / (amount_add / current_value + share_hold_now) 
-        rate_of_hold = ((amount_add + share_hold_now * current_value) / (amount_add + total_now) - 1) * 100 
+#        print(amount_add *(1 - purchase_fee /100) / current_value)
+        cost_hold = (total_now + amount_add) * (1 - purchase_fee / 100) / ( round((amount_add *(1 - purchase_fee /100) / current_value),2) + share_hold_now)
+        cost_hold = round(cost_hold, 4)
+        rate_of_hold = (( round((amount_add * (1 - purchase_fee / 100)), 2) + share_hold_now * current_value) / (amount_add + total_now) * (1 - purchase_fee / 100) - 1) * 100 
         if rate_of_hold < 0:
-            rate_of_hold = math.fabs(rate_of_hold)
-        rate_of_return = rate_of_hold / 100 / (1 - rate_of_hold / 100) * 100
+            rate_of_return = math.fabs(rate_of_hold / 100 / (1 + rate_of_hold / 100) * 100)
+        else:
+            rate_of_return = 0
+        #print(amount_add,cost_hold,rate_of_hold,rate_of_return)
         print("加仓金额：%.0f " % amount_add, "预计持仓成本：%.4f " % cost_hold, "预计持有收益率：%.2f" % rate_of_hold + '% ', "预计回本需涨：%.2f" % rate_of_return + "% ")
 
 def main():
@@ -30,6 +35,7 @@ def main():
     parser.add_argument('--max', default=10000, help='最大加仓金额')
     parser.add_argument('--step', default=1000, help='加仓金额步进')
     parser.add_argument('-c', '--current_value', default=None, help='预计当日净值')
+    parser.add_argument('-p', '--purchase_fee', default=0, help='买入手续费')
     parser.add_argument('-t', '--total_now', default=None, help='已买入的总金额')
     parser.add_argument('-s', '--share_hold_now', default=None, help='当前持有份额')
     parser.add_argument('-m', '--mode', default=None, help='模式选择')
@@ -42,6 +48,7 @@ def main():
     max = args.max
     step = args.step
     current_value = args.current_value
+    purchase_fee = args.purchase_fee
     total_now = args.total_now
     share_hold_now = args.share_hold_now
     mode = args.mode
@@ -56,7 +63,7 @@ def main():
 
         if mode == "2":
             if current_value and total_now and share_hold_now:
-                cost_hold_method(int(min), int(max), int(step), float(current_value), float(total_now), float(share_hold_now))
+                cost_hold_method(int(min), int(max), int(step), float(current_value), float(purchase_fee), float(total_now), float(share_hold_now))
             else:
                 print("min(default=1000), max(default=10000), step(default=1000), current_value, total_now, share_hold_now 需要同时指定")    
     else:
